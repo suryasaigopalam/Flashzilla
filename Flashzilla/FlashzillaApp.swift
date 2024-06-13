223319cd8791959ce7218c15d681b04caf5f2694
suryasai <suryasaigopalam@gmail.com>
2024-06-13 11:37:13 +0530

    Initial Commit

---
+++ Flashzilla/FlashzillaApp.swift
@@ -0,0 +1,17 @@
+//
+//  FlashzillaApp.swift
+//  Flashzilla
+//
+//  Created by surya sai on 13/06/24.
+//
+
+import SwiftUI
+
+@main
+struct FlashzillaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
